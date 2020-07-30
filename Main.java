905c7f9e25bd93f3aa6c66ecf82c83affa936697
Slava Maratkanov <slava_maratkanov@mail.ru>
2020-07-30 20:31:14 +0500

    Worker

--- Main.java
+++ Main.java
@@ -1,13 +1,9 @@
 public class Main {
 
-    public static void main(String[] args){
-        Calculator calc = Calculator.instance.get();
+   private static Worker worker = new Worker();
 
-        int a = calc.plus.apply(1, 2);
-        int b = calc.minus.apply(2,8);
-        int c = calc.devide.apply(a, b);
-        int result = calc.abs.apply(c);
-
-        calc.println.accept(result);
+    public static void main(String[] args) {
+        OnTaskDoneListener listener = System.out::println;
+        worker.start();
     }
 }
